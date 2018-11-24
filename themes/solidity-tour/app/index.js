9eb0671f423b18ddb367c29bbbebef40418f875a
Erik Kundt <bitshift@posteo.org>
2018-11-24 22:56:10 +0100

    Moves steps to slider.

--- themes/solidity-tour/app/index.js
+++ themes/solidity-tour/app/index.js
@@ -8,16 +8,14 @@ class Tour extends React.Component {
     
     const wrapper = require('solc/wrapper');
     
-    this.state = { solc: wrapper(window.Module), code: "", output: "", compiled: false, success: false };
+    this.state = { solc: wrapper(window.Module), id: props.id, code: "", output: "", compiled: false, success: false };
   }
 
   compile() {
-    const { solc, output } = this.state;
+    const { solc, id } = this.state;
 
-    const code = window.editor.getValue();
+    const code = window.editors[id].getValue();
     const compilation = solc.compile(code);
-
-    console.log(compilation);
 
     const hasErrors = (typeof compilation.errors !== 'undefined');
     const compilationOutput = hasErrors ? compilation.errors[0] : "Compilation successful.";
@@ -42,11 +40,17 @@ class Tour extends React.Component {
         'Compile'
     );
     
-    const controls = e('div', { className: "stage" }, [btn_compile]);
+    const controls = e('div', { className: "stage" }, btn_compile);
     
     return [error, controls];
   }
 }
 
-const domContainer = document.querySelector('#compilation');
-ReactDOM.render(e(Tour), domContainer);
+
+var reactElements = document.querySelectorAll(".compilation");
+var step = 0;
+reactElements.forEach(function(element) {
+  ReactDOM.render(e(Tour, {id: step}), element);
+  step++;
+});
+
